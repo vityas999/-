bbe8d1545987377fa14918643b393e61f77a538e
Vitalik <vitalijzaxarov99@mail.ru>
2023-03-08 14:03:34 +0300

    Внесли синтаксические корректировки в Task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -2,23 +2,23 @@
 // a = 5; b = 7 -> max = 7
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
-Console.Write("Введите первое целое число ");
+Console.Write("Введите первое целое число: ");
 int number1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите второе целое число ");
+Console.Write("Введите второе целое число: ");
 int number2 = Convert.ToInt32(Console.ReadLine());
 if (number1 > number2)
 {
-    Console.WriteLine("max " + number1);
+    Console.WriteLine("max = " + number1);
 }
 else
 {
-    Console.WriteLine("min " + number1);
+    Console.WriteLine("min = " + number1);
 }
 if (number2 > number1)
 {
-    Console.WriteLine("max " + number2);
+    Console.WriteLine("max = " + number2);
 }
 else
 {
-    Console.WriteLine("min " + number2);
+    Console.WriteLine("min = " + number2);
 }
