1641aa6eb772a7c4d8da15701deac96da25c54d8
Vitalik <vitalijzaxarov99@mail.ru>
2023-03-11 22:57:32 +0300

    Добавили инфу в Task10

---
+++ Task10/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+int secondDigit(int num)
+{
+int thirdDigit = num % 100;
+int result = thirdDigit / 10;
+return result;
+}
+int number = new Random().Next(100,1000);
+int rsd = secondDigit(number);
+Console.WriteLine($"Случайное число из отрезка 100 - 999 -> {number}");
+Console.WriteLine($"Число -> {rsd}");
