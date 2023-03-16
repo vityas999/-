b2c75b2dc454bd049d17c0d3b84301f074095e40
Vitalik <vitalijzaxarov99@mail.ru>
2023-03-16 19:01:40 +0300

    Добавили код в Task23

---
+++ Task23/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 5 ->
+// 1 | 1
+// 2 | 8
+// 3 | 27
+// 4 | 64
+// 5 | 125
+Console.WriteLine("Введите натуральное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+TableCube(num);
+
+void TableCube(int number)
+{
+int count = 1;
+while (count <= number)
+  {
+    int cube = count * count * count;
+    Console.WriteLine($"{count, 5} | {cube, 5}");
+    count++;
+  }
+
+}
