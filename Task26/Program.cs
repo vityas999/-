cac9bc613ac1a41279a4f310db82ea601e7c0d53
Vitalik <vitalijzaxarov99@mail.ru>
2023-03-17 11:05:31 +0300

    Добавили код в Task26

---
+++ Task26/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 26: Напишите программу, которая принимает
+// на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+Console.WriteLine("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int sumNumbers = SumNumbers(number);
+Console.WriteLine($"Сумма чисел от 1 до {number} = {sumNumbers}");
+
+int SumNumbers(int num)
+{
+int count = 0;
+while (num > 0)
+{
+   num = num/10;
+   count++;
+}
+return count;
+}
