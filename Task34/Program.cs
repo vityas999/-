f736ac4900b007dc32dc5033835bcf32ccc1a769
Vitalik <vitalijzaxarov99@mail.ru>
2023-03-25 16:08:18 +0300

    Добавили папку Task34

--- Task34/Program.cs
+++ Task34/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами.
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+int[] CreateArray(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]},");
+    }
+    Console.Write("]");
+}
+
+int EveNumbersArray(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0)
+            ++count;
+    }
+    return count;
+}
+
+
+
+int[] array = CreateArray(4,100,999);
+PrintArray(array);
+int eveNumbersArray = EveNumbersArray(array);
+Console.WriteLine($" - четных чисел в массиве: {eveNumbersArray}");
