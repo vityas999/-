229f6c33a9cfdca9a68b11254a0c305620e868ab
Vitalik <vitalijzaxarov99@mail.ru>
2023-03-26 21:26:13 +0300

    Добавили папку Task41

---
+++ Task41/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+int lenght = Input("Введите количество элементов: ");
+int[] array;
+array = InputArr(lenght);
+PrintArray(array);
+Console.WriteLine($"Количество чиссел больше 0: {PositiveNumbersCount(array)}");
+
+int Input(string massage)
+
+{
+    Console.Write(massage);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+int[] InputArr(int lenght)
+{
+    int[] array = new int[lenght];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Input($"Введите {i + 1} - й элемент: ");
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine($"[{i}] = {array[i]}");
+    }
+}
+
+int PositiveNumbersCount(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+            count++;
+    }
+    return count;
+}
