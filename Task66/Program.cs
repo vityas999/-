1b8d21bc7840206475f546d44d5f5ede3f0c20f0
Vitalik <vitalijzaxarov99@mail.ru>
2023-04-08 16:58:28 +0300

    Добавили папку Task64 и Task66

---
+++ Task66/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 66: Задайте значения M и N. 
+// Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+Console.Write("Введите значение M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int summa(int m, int n)
+{
+    if (m == n)
+        return n;
+    else return m + summa(m + 1, n);
+}
+Console.Write($"Сумма натуральных элементов в промежутке от M до N = {summa(m, n)}");
